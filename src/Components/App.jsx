4059edd4ce0d13d7bb0a7b9ab637ea4a92105e12
Aryadip Mridha <mearjo1999@gmail.com>
2023-11-06 19:48:54 +0530

    initialisation componenents lol

---
+++ src/Components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./Footer";
+import Note from "./Note";
+import Header from "./Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+  );
+}
+export default App;
---
+++ src/Components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Header() {
+  return (
+    <div className="note">
+      <h1>A thing</h1>
+      <p>to remind</p>
+    </div>
+  );
+}
+export default Header;
